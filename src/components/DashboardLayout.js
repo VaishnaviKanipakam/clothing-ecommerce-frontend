3e808d2f7353dc154697d56a9ffe7ca27d7c4056
VaishnaviKanipakam <kanipakamvaishnavi1998@gmail.com>
2025-12-27 16:53:41 +0530

    some updates are added

--- src/components/DashboardLayout.js
+++ src/components/DashboardLayout.js
@@ -1,11 +1,11 @@
 import DashboardHeader from "./DashboardHeader";
-import { useEffect, useContext } from "react";
+import { useEffect, useState } from "react";
 import CartContext from "../context/CartContext";
 import Cookies from "js-cookie";
 import Footer from "./Footer";
 
 const DashboardLayout = ({ children }) => {
-  const { cartCount, setCartCount } = useContext(CartContext);
+  const [cartCount, setCartCount] = useState(0); 
   const jwtToken = Cookies.get("jwt_token");
   const user = JSON.parse(localStorage.getItem("user"));
   const userId = user?.result?.[0]?.user_id;
